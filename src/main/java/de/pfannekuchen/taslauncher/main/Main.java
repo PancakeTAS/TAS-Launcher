fd105a1e562f0f12b4447a884f020e61d27945ea
Pfannekuchen <gamesupport@mgnet.work>
2021-06-25 12:38:30 +0200

    Update Account Api & Redirect Output Streams
    I updated the Account Api and it works with exports too now. Additionally, since jpackage does not give an output stream, I redirected that too

--- src/main/java/de/pfannekuchen/taslauncher/main/Main.java
+++ src/main/java/de/pfannekuchen/taslauncher/main/Main.java
@@ -1,11 +1,20 @@
 package de.pfannekuchen.taslauncher.main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 import de.pfannekuchen.taslauncher.TASLauncher;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+		// Move Streams
+		System.setOut(new PrintStream(new File(System.getProperty("user.home"), "launcher_out.log")));
+		System.setErr(new PrintStream(new File(System.getProperty("user.home"), "launcher_err.log")));
 		TASLauncher.start(); // Start JavaFX
+		System.out.close();
+		System.err.close();
 	}
 
 }
